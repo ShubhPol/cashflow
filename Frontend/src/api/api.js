291a00d1c178c83816fcee98138477f6932564a8
ShubhangiPol <shubhangi.pol1211@gmail.com>
2026-06-04 20:05:24 +0530

    connect frontend to backend

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://your-backend-name.onrender.com",
+  baseURL: "https://cashflow-backend-xm62.onrender.com",
 });
 
 export default API;
